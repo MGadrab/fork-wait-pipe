1e528492e6a198b87373b11c1f94dbd66c556706
MGadrab <mgadrab@gmail.com>
2025-04-06 15:00:21 -0500

    reorganized and started usage of pipes and forks

--- usage_of_pipes_forks.h
+++ usage_of_pipes_forks.h
@@ -12,33 +12,51 @@
 int pipes_and_forks(){
     int arr[]= {1,3 ,4, 5,2,3};
     int arrSize = sizeof(arr)/ sizeof(int); /// have to divide by size of int b/c sizeof returns bytes
-
     int start, end;
-    int value;
+    int TotalVal;
     int fd[2];
-    int id = fork();
     if (pipe(fd) < 0){ /// signifies error in the pipe
         return 1;
     }
+    int id = fork();
     if(id < 0){ /// error in the fork
         return 2;
     }
     if(id == 0){ /// id 0 means that it is a child
         start = 0;
-        end = start + arrSize/2; /// the end is the middle array, dont need to add 'start' as start is 0
-        for(int i = start;i < end; i++){
-            value = value + arr[i];
-        }
-        printf("value; %d\n", value);
+        end = arrSize/2; /// the end is the middle array, dont need to add 'start' as start is 0
     }
     else{
         start = arrSize/2; /// start where the child ends
         end = arrSize;
-        for(int i = start;i < end; i++){
-            value = value + arr[i];
-        }
-        printf("value; %d\n", value);
+    }
+    int value = 0;
+    for(int i = start;i < end; i++){ /// you can have this outside the if/else cause the loop is the same for both
+
+        value+= arr[i];
+    }
+    printf("value: %d\n", value);
+
+    /// now that we have value for each we need to use pipe to send values
+    if(id == 0){ /// write in child
+        close(fd[0]);
+        write(fd[1], &value, sizeof(value));
+        close(fd[1]);
+    }
+    else{
+        wait(NULL);
+        int ValueFromChild;
+        close(fd[1]);
+        read(fd[0], &ValueFromChild, sizeof(ValueFromChild));
+        close(fd[0]);
+        TotalVal = value + ValueFromChild;
+        printf("Total Value: %d", TotalVal);
+         /// parent needs to wait for child or ELSE!! (if parent goes first cannot add value of child)
     }
     return 0;
 }
+/*
+ a mistake I was doing while trying this is that I used pipe AFTER fork.
+ have to use it BEFORE so that all processes share the same pipe file descriptor
+*/
 #endif //FORK_WAIT_PIPE_USAGE_OF_PIPES_FORKS_H
